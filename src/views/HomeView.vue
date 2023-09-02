8f7a1786fde0748ca5c136b115fe3fb039780cf1
Aniket Aryamane <aryamane.aniket@gmail.com>
2023-09-02 11:29:06 +0530

    Basic HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,48 @@
+<script setup>
+import carsData from '@/assets/data.json'
+import { ref } from 'vue'
+const cars = ref(carsData)
+</script>
+
 <template>
-    <div>
-        <h1>Home</h1>
-    </div>
+    <main class="container">
+        <h1>Our Cars</h1>
+        <div class="cards">
+            <div
+                v-for="car in cars"
+                :key="car.id"
+                class="card"
+            >
+                <h2>{{ car.make }}</h2>
+                <p>{{ car.price }}</p>
+            </div>
+        </div>
+    </main>
 </template>
+
+<style scoped>
+.cards {
+    display: flex;
+    width: 1000px;
+    flex-wrap: wrap;
+    margin-top: 50px;
+    justify-content: center;
+}
+
+.card {
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.207);
+    padding: 15px;
+    width: 150px;
+    margin-right: 15px;
+    cursor: pointer;
+    margin-bottom: 20px;
+}
+
+.links {
+    padding: 20px;
+}
+
+.links a {
+    margin: 0 5px;
+}
+</style>
