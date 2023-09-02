22395b9873b65f48b43ffb06698bf696ad6bfea9
Aniket Aryamane <aryamane.aniket@gmail.com>
2023-09-02 11:16:41 +0530

    Adding vue-router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import App from '@/App.vue'
+import '@/assets/css/main.css'
+import router from '@/router'
 import { createApp } from 'vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
